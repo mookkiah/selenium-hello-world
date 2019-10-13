746e3d9e425e82ede81664a86f2a24c87d6ad079
Mahendran Mookkiah <mahendran.mookkiah@cevalogistics.com>
2019-10-13 04:38:47 -0400

    using WebDriverManager

--- setup.sh
+++ setup.sh
@@ -3,10 +3,7 @@
 curl https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar -o selenium-server-standalone.jar
 
 #Download chromedriver for your operating system
-curl https://chromedriver.storage.googleapis.com/73.0.3683.20/chromedriver_linux64.zip -o chromedriver.zip
-#curl https://chromedriver.storage.googleapis.com/73.0.3683.20/chromedriver_mac64.zip -o chromedriver.zip
-unzip chromedriver.zip
-rm chromedriver.zip
+mvn exec:java -Dexec.args="chrome"
 
 #Download geckodriver for your operating system
 #curl https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-macos.tar.gz -o geckodriver.tar.gz
