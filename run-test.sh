90390359ff97065d05ab8a191d4b2d46c2754464
Mahendran Mookkiah <mahendran.mookkiah@cevalogistics.com>
2019-10-21 15:28:19 -0400

    commented template export

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-export SAUCE_USERNAME="your-username"
-export SAUCE_ACCESS_KEY="your-access-key"
+#export SAUCE_USERNAME="your-username"
+#export SAUCE_ACCESS_KEY="your-access-key"
+
 
 mvn clean test -DhubUrl=http://localhost:4444/wd/hub
 #mvn clean test -DhubUrl=https://ondemand.saucelabs.com:443/wd/hub
