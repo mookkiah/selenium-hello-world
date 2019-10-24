dbaae4dcf9d6e1859963e830589a8b981071995e
Mahendran Mookkiah <mahendran.mookkiah@gmail.com>
2019-10-24 13:55:20 -0400

    using mvnw instead of mvn

--- setup.cmd
+++ setup.cmd
@@ -2,7 +2,7 @@ set path=%path%;%JAVA_HOME%\bin
 
 
 rem Download chromedriver for your operating system
-mvn exec:java -Dexec.args="chrome"
-mvn exec:java -Dexec.args="iexplorer"
-mvn exec:java -Dexec.args="firefox"
-mvn exec:java -Dexec.args="edge"
+./mvnw exec:java -Dexec.args="chrome"
+./mvnw exec:java -Dexec.args="iexplorer"
+./mvnw exec:java -Dexec.args="firefox"
+./mvnw exec:java -Dexec.args="edge"
--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 curl https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar -o selenium-server-standalone.jar
 
 #Download chromedriver for your operating system
-mvn exec:java -Dexec.args="chrome"
-mvn exec:java -Dexec.args="iexplorer"
-mvn exec:java -Dexec.args="firefox"
+./mvnw exec:java -Dexec.args="chrome"
+./mvnw exec:java -Dexec.args="iexplorer"
+./mvnw exec:java -Dexec.args="firefox"
 
