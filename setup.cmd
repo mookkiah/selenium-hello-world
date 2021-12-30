c70f3da119177672669361d1841678b4bb550606
Mahendran Mookkiah <mahendran.mookkiah@gmail.com>
2021-12-30 09:21:18 -0500

    small improvements.

--- setup.cmd
+++ setup.cmd
@@ -1,8 +1,8 @@
-set path=%path%;%JAVA_HOME%\bin
+REM TODO: if java is not in path
+REM set path=%path%;%JAVA_HOME%\bin
 
-
-rem Download chromedriver for your operating system
-./mvnw exec:java -Dexec.args="chrome"
-./mvnw exec:java -Dexec.args="iexplorer"
-./mvnw exec:java -Dexec.args="firefox"
-./mvnw exec:java -Dexec.args="edge"
+REM Download chromedriver for your operating system
+mvnw exec:java -Dexec.args="chrome"
+mvnw exec:java -Dexec.args="iexplorer"
+mvnw exec:java -Dexec.args="firefox"
+mvnw exec:java -Dexec.args="edge"
--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 curl https://selenium-release.storage.googleapis.com/4.0-beta-2/selenium-server-4.0.0-beta-2.jar -o selenium-server.jar
 
 #Download chromedriver for your operating system
-./mvnw exec:java -Dexec.args="chrome"
+./mvnw exec:java -Dexec.args="chrome" #-Dwdm.chromeDriverVersion=89
 ./mvnw exec:java -Dexec.args="iexplorer"
-./mvnw exec:java -Dexec.args="firefox"
+./mvnw exec:java -Dexec.args="firefox" # -Dwdm.geckoDriverVersion=0.26.0
+./mvnw exec:java -Dexec.args="edge" # -Dwdm.edgeDriverVersion=81.0.410.0
 
